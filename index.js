e597322aa80e868dc8f27e06df2cb1921c4f149b
apporoad <apporoadina@163.com>
2019-11-23 10:56:18 +0800

    part

---
+++ index.js
@@ -0,0 +1,51 @@
+const git = require('simple-git/promise')
+const utils = require('lisa.utils')
+const download = require('download')
+const fs =require('fs')
+let { zip, unzip } = require('cross-unzip')
+const rimraf = require('rimraf')
+
+
+exports.getRepo= (src,workspace,type)=>{
+    return new Promise((r,j)=>{
+        if(fs.existsSync(workspace))
+        {
+            r('workspace already exists ：' + workspace)
+            return
+        }
+        if(type == 'git'){
+            r(git().silent(true).clone(src,workspace))
+        }else if(type == 'zip'){
+            if(utils.startWith(src,'http')){
+                r(download(src, process.cwd() + '/dlTemp.zip').then(() => {
+                    return new Promise((r1,j1)=>{
+                        unzip(process.cwd() + '/dlTemp.zip', workspace, err => {
+                            if(err)
+                            {
+                                j1('something wrong with your zip')
+                            }else
+                            {
+                                r1()
+                            }
+                          })
+                    })
+                }).then(()=>{
+                    rimraf(process.cwd() + '/dlTemp.zip',()=>{})
+                }))
+            }else{
+                unzip(src, workspace, err => {
+                    if(err)
+                    {
+                        j('something wrong with your zip')
+                    }else
+                    {
+                        r()
+                    }
+                  })
+            }
+        }
+        else{
+            j('type not supported')
+        }
+    })
+}
---
+++ test.js
@@ -0,0 +1,15 @@
+var dl =require('./index')
+
+
+// dl.getRepo('https://github.com/apporoad/lisa.dl.js',__dirname + '/temp','git').then(r=>{
+//     console.log(r || 'hello')
+// })
+
+// dl.getRepo('https://github.com/apporoad/aok.js/blob/master/example/example.zip?raw=true',__dirname+'/temp','zip').then(r=>{
+//          console.log(r || 'hello')
+// })
+
+
+dl.getRepo(__dirname+'/example.zip',__dirname+'/temp','zip').then(r=>{
+         console.log(r || 'hello')
+})
